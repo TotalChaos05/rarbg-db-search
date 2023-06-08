7b24efd5a712648b758624c28340304d184b7442
Basil Keeler <basil.keeler@outlook.com>
2023-06-08 16:08:53 -0500

    added cli

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use rusqlite::{Connection, Result};
 use itertools::Itertools;
 use serde::{Serialize};
+use std::env::args;
+use std::fs::File;
+use std::io::Write;
 pub trait StringUpper{
     fn as_upper(&self) -> String;
 }
@@ -30,9 +33,11 @@ struct Entry {
     ext_id: Option<String>,
     imdb: Option<String>,
 }
+impl Entry{
+}
 fn load(conn: Connection) -> Result<Vec<Entry>>{
     let mut stmt = conn.prepare("SELECT id,hash,title,dt,cat,size,ext_id,imdb FROM items").unwrap();
-    let person_iter = stmt.query_map([], |row| {
+    let entries = stmt.query_map([], |row| {
         Ok(Entry {
             id: row.get(0)?,
             hash: row.get(1)?,
@@ -43,9 +48,10 @@ fn load(conn: Connection) -> Result<Vec<Entry>>{
             ext_id: row.get(6)?,
             imdb: row.get(7)?,
         })
-    })?;
+    })?.map(|i| i.unwrap()).collect();
 
-    Ok(person_iter.map(|i| i.unwrap()).collect())
+
+    Ok(entries)
 }
 
 fn search(entries: &Vec<Entry>,query: Vec<String>) -> Vec<Entry>{
@@ -63,12 +69,40 @@ fn imdb_search(entries: &Vec<Entry>,query: String) -> Vec<Entry>{
     out.into_iter().unique().collect()
 }
 
+fn help(){
+        println!("RARBG Database Search tool
+Usage:
+        rarbg-search [OPTIONS] [QUERY]
+Options:
+        -h --help:      show this help document
+        -i --imdb:      search with imdb id
+        no arguments:   plaintext search")
+
+}
 
 fn main() {
-    println!("Hello, world!");
     let conn = Connection::open("rarbg_db.sqlite").unwrap();
     let entries = load(conn).unwrap();
-    println!("{}",serde_json::to_string_pretty(&search(&entries,vec!["queer".into()])).unwrap());
-    println!("{}",serde_json::to_string_pretty(&imdb_search(&entries,"tt6718170".into())).unwrap());
+    let args: Vec<String> = args().collect();
+    if args.contains(&"-h".to_string()) || args.contains(&"--help".to_string()){
+        help()
+    }else if args.len() > 3 || args.len() == 1 {help()}
+    else if args[1] == "-i" || args[1] == "--imdb"{
+        let out: Vec<(String,String)> = imdb_search(&entries, args[2].clone()).into_iter().map(|entry| (entry.title,entry.hash)).collect();
+        for entry in out{
+            println!("Title:    {}",entry.0);
+            println!("Link:     magnet:?xt.1={}",entry.1);
+            println!("");
+        }
 
+    }else {
+        let out: Vec<(String,String)> = search(&entries,args[1..].into()).into_iter().map(|entry| (entry.title,entry.hash)).collect();
+        for entry in out{
+            println!("Title:    {}",entry.0);
+            println!("Link:     magnet:?xt.1={}",entry.1);
+            println!("");
+
+    }
+
+    }
 }
